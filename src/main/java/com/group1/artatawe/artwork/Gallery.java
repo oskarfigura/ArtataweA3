66890e3383539d09b6236311d235119bb79aebf2
Kristiyan Vladimirov <28785463+krisvladimirov@users.noreply.github.com>
2018-03-07 14:53:18 +0000

    Added methods that would load/write to json files

--- src/main/java/com/group1/artatawe/artwork/Gallery.java
+++ src/main/java/com/group1/artatawe/artwork/Gallery.java
@@ -1,15 +1,21 @@
 package com.group1.artatawe.artwork;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.group1.artatawe.Main;
 import com.group1.artatawe.accounts.Account;
-import com.group1.artatawe.listings.Listing;
-
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import com.group1.artatawe.listings.Listing;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Represents a gallery of artworks curated by a user
  * @author Adam Payne
- * @version 1.0.1
+ * @version 1.0
  */
 public class Gallery {
     //Properties
@@ -31,10 +37,72 @@ public class Gallery {
      * Read up and construct a gallery
      * @param jo
      */
-    public Gallery(JsonObject jo) {
-        //TODO -> sort out the JSON
+    public Gallery(JsonObject jo, Account acc) {
+        this.loadFromJson(jo, acc);
     }
 
+    public JsonObject toJsonObject() {
+        JsonObject jo = new JsonObject();
+
+        jo.addProperty("userName", this.user.getUserName());
+        jo.addProperty("galleryName", this.name);
+
+        JsonArray galleryArray = new JsonArray();
+        /*
+        Previous method using artworks instead of listings
+        
+        this.listings.stream().forEach(x -> {
+            Main.listingManager.getAllListings().stream().forEach(y -> {
+                if (y.getArtwork() == x) {
+                    int id = y.getID();
+                    galleryArray.add(id);
+                }
+            });
+        });
+        */
+
+        this.listings.stream().forEach(x -> {
+            int id = x.getID();
+            galleryArray.add(id);
+        });
+        jo.add("list", galleryArray);
+
+        return jo;
+    }
+
+    /**
+     * Loads a user gallery from a JsonObject
+     * @param jo
+     */
+    public void loadFromJson(JsonObject jo, Account acc) {
+        try {
+            this.user = acc;
+        } catch (NullPointerException e) {
+            System.out.println("Ops no account, couldn't load gallery");
+        }
+        this.name = jo.get("galleryName").getAsString();
+
+        /*
+
+        Iterator<JsonElement> it = jo.get("list").getAsJsonArray().iterator();
+        it.forEachRemaining(x -> {
+            int id = x.getAsInt();
+            Main.listingManager.getAllListings().stream().forEach(y -> {
+                if (y.getID() == id) {
+                    this.listings.add(y.getArtwork());
+                }
+            });
+        });
+        */
+
+        Iterator<JsonElement> it = jo.get("list").getAsJsonArray().iterator();
+        it.forEachRemaining(x -> {
+            int id = x.getAsInt();
+            Listing l = Main.listingManager.getListing(id);
+            this.listings.add(l);
+        });
+    }
+    
     /**
      * Get the user who created the gallery
      * @return The account of the user who created the gallery
@@ -53,7 +121,7 @@ public class Gallery {
 
     /**
      * Get the listings contained in the gallery
-     * @return The listings in the gallery, in an ArrayList
+     * @return The artworks in the gallery, in an ArrayList
      */
     public ArrayList<Listing> getListings() {
         return listings;
@@ -61,7 +129,7 @@ public class Gallery {
 
     /**
      * Add a listing to the gallery
-     * @param listing The listing to be added to the gallery
+     * @param listing The artwork to be added to the gallery
      */
     public void addListing(Listing listing) {
         listings.add(listing);
@@ -69,7 +137,7 @@ public class Gallery {
 
     /**
      * Remove a listing from the gallery
-     * @param listing The listing to be removed from the gallery
+     * @param listing
      */
     public void removeListing(Listing listing) {
         listings.remove(listing);
