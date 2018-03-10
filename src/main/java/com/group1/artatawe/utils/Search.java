8b57b5e0e1a68edb3485d63a77cf4c68061d826e
Yousef Al-Mal <yousef@yousefs-mbp.home>
2018-03-10 18:04:05 +0000

    Searching class

--- src/main/java/com/group1/artatawe/utils/Search.java
+++ src/main/java/com/group1/artatawe/utils/Search.java
@@ -1,13 +1,54 @@
 package com.group1.artatawe.utils;
-
+import java.util.regex.*;
+import java.util.List;
+import com.group1.artatawe.artwork.Artwork;
+import com.group1.artatawe.listings.Listing;
+import java.util.ArrayList;
 import com.group1.artatawe.managers.ListingManager;
 
 public class Search {
-    public static void main(String[] args) {
 
-        String search = "Jesus Sculpture";  //users search keyword example for testing
+    public static ArrayList<Artwork> searchForDetails(String s){
+
         ListingManager listingManager = new ListingManager();
-        listingManager.getAllActiveListings();
+       // listingManager.getAllActiveListings().stream().filter(x-> x.getArtwork().getDescription().contains("jesus"));
+        //listingManager.getAllActiveListings().stream().filter(x-> x.getArtwork().getTitle().contains("jesus"));
+        List<Listing> lists = listingManager.getAllActiveListings();
+
+        String pattern = "(.*)(.*" +s +".*)(.*)";
+        ArrayList<Artwork> found = new ArrayList<>();
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+        Matcher m;
+        for (Listing L:lists) {
+            m = r.matcher(L.getArtwork().getTitle());
+            if(m.find()) found.add(L.getArtwork());
+            m = r.matcher(L.getArtwork().getDescription());
+            if(m.find())  found.add(L.getArtwork());
+        }
+
+        // Now create matcher object.
+      /*
+        if (m.find( )) {
+            return ("Found value: " + m.group(0) );
+            //System.out.println("Found value: " + m.group(1) );
+            //System.out.println("Found value: " + m.group(2) );
+        } else {
+            return ("NO MATCH");
+        }
+*/      return found;
+
+    }
+    public static void main(String[] args) {
+        String line = "jesus sculpture";
+        ArrayList<Artwork> art1 = searchForDetails("adam");
+        ArrayList<Artwork> art2 =  searchForDetails("jESus");
+        for(Artwork a:art1)
+            System.out.println(a.getTitle());
+
+        for(Artwork a:art2)
+            System.out.println(a.getTitle());
+
     }
 
 }
