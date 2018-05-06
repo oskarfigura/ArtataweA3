9def8c399d40396d63d4e3907a87f35c2ef55b3f
Oskar <ofigura@interia.pl>
2018-05-06 10:59:36 +0100

    Finished MessageHistory class

--- src/main/java/com/group1/artatawe/managers/MessageManager.java
+++ src/main/java/com/group1/artatawe/managers/MessageManager.java
@@ -1,10 +1,10 @@
 package com.group1.artatawe.managers;
 
-import com.group1.artatawe.utils.Review;
+import com.group1.artatawe.messages.Conversation;
 
 import java.util.LinkedList;
 
 public class MessageManager {
     private static final String MESSAGE_FILE = "messages.json";
-    private final LinkedList<Message> messages = new LinkedList<>();
+    private final LinkedList<Conversation> messages = new LinkedList<>();
 }
